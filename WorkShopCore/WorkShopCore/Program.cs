62433c387c9ffb7af45d9328d69d3eefc0f77b20
Franco Morales <franco.e.morales@gmail.com>
2018-07-27 14:33:48 -0300

    firs t commit

---
+++ WorkShopCore/WorkShopCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WorkShopCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
